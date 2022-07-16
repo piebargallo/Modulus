aad335bc066a65137d8481895ddea36efce29d15
piebargallo <90567646+piebargallo@users.noreply.github.com>
2022-07-16 11:20:02 +0200

    Add files via upload

---
+++ Modulus.cpp
@@ -0,0 +1,16 @@
+// CPP program to find modulo of floating
+// point numbers using library function.
+#include <bits/stdc++.h>
+using namespace std;
+
+// Driver Function
+int main() {
+	
+	// Def variables
+	double a = 9.7, b = 2.3;
+	
+	// Use fmod function
+	cout << "Modulus of a and b: " << fmod(a, b);
+	
+	return 0;
+} //End driver
